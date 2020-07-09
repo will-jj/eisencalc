3d40ee2113df4f4aedddf01e9dc0245457376b53
to-metrion <20890062+to-metrion@users.noreply.github.com>
2020-07-09 16:55:58 -0400

    Update move_data.js

--- _scripts/move_data.js
+++ _scripts/move_data.js
@@ -4803,7 +4803,7 @@ var MOVES_SS = $.extend(true, {}, MOVES_SM, {
 		"type": "Ice",
 		"category": "Physical",
 		"makesContact": true,
-		"isMultihit": 3,
+		"isMultihit": true,
 		"acc": 90,
 	},
 	"Scorching Sands": {
