ebee854d289de9f340c12b75588ac6e9cc24202e
to-metrion <20890062+to-metrion@users.noreply.github.com>
2020-07-11 19:49:23 -0400

    Updated P-Z EVs

--- _scripts/setdex_gen8_sets.js
+++ _scripts/setdex_gen8_sets.js
@@ -19480,10 +19480,11 @@ var SETDEX_GEN8_SETS = {
 				"hp": 0,
 				"at": 0,
 				"df": 0,
-				"sa": 0,
+				"sa": 252,
 				"sd": 0,
 				"sp": 0
 			},
+			"nature": "Modest",
 			"ability": "Download",
 			"item": "Normal Gem",
 			"moves": [
