9843689fa4d90579e7817167406fa54dabbe3a3b
to-metrion <20890062+to-metrion@users.noreply.github.com>
2020-07-14 17:16:05 -0400

    Update setdex_gen8_sets.js

--- _scripts/setdex_gen8_sets.js
+++ _scripts/setdex_gen8_sets.js
@@ -8767,12 +8767,13 @@ var SETDEX_GEN8_SETS = {
 			"level": "50",
 			"evs": {
 				"hp": 0,
-				"at": 0,
+				"at": 252,
 				"df": 0,
 				"sa": 0,
 				"sd": 0,
 				"sp": 0
 			},
+			"nature": "Adamant",
 			"ability": "Defiant",
 			"item": "Rocky Helmet",
 			"moves": [
