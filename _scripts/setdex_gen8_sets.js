0624d996708feb3cdfc68a01940f7b48e0722ec0
to-metrion <20890062+to-metrion@users.noreply.github.com>
2020-07-17 01:31:02 -0400

    Bisharp-RS

--- _scripts/setdex_gen8_sets.js
+++ _scripts/setdex_gen8_sets.js
@@ -2407,12 +2407,13 @@ var SETDEX_GEN8_SETS = {
 			"level": "50",
 			"evs": {
 				"hp": 0,
-				"at": 0,
+				"at": 252,
 				"df": 0,
 				"sa": 0,
 				"sd": 0,
 				"sp": 0
 			},
+			"nature": "Adamant",
 			"ability": "Defiant",
 			"item": "Black Glasses",
 			"moves": [
