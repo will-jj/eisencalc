64f4ff4e21b65f6247ab70051bc9232554d19863
to-metrion <20890062+to-metrion@users.noreply.github.com>
2021-01-15 05:59:48 -0500

    2nd try

--- _scripts/ap_calc.js
+++ _scripts/ap_calc.js
@@ -942,7 +942,6 @@ function getMoveDetails(moveInfo, item, species) {
 		var exceptions_100_fight = ["Low Kick", "Reversal", "Final Gambit"];
 		var exceptions_80_fight = ["Double Kick", "Triple Kick"];
 		var exceptions_75_fight = ["Counter", "Seismic Toss"];
-		var exceptions_150 = ["Head Smash"];
 		var exceptions_140 = ["Triple Axel", "Crush Grip", "Wring Out", "Magnitude", "Double Iron Bash", "Rising Voltage"];
 		var exceptions_130 = ["Scale Shot", "Dual Wingbeat", "Terrain Pulse", "Bolt Beak (Doubled)", "Fishious Rend (Doubled)", "Pin Missile", "Power Trip", "Punishment", "Dragon Darts", "Dual Chop", "Electro Ball", "Heat Crash",
 			"Bullet Seed", "Grass Knot", "Bonemerang", "Bone Rush", "Fissure", "Icicle Spear", "Sheer Cold", "Weather Ball", "Tail Slap", "Guillotine", "Horn Drill",
@@ -964,12 +963,12 @@ function getMoveDetails(moveInfo, item, species) {
 			else if (defaultDetails.bp >= 150 || exceptions_100_fight.includes(moveName)) tempBP = 100;
 			else tempBP = 70;
 		} else {
-			if (defaultDetails.bp >= 110 || exceptions_140.includes(moveName)) tempBP = 140;
+			if (defaultDetails.bp >= 150) tempBP = 150;
+			else if (defaultDetails.bp >= 110 || exceptions_140.includes(moveName)) tempBP = 140;
 			else if (defaultDetails.bp >= 75 || exceptions_130.includes(moveName)) tempBP = 130;
 			else if (defaultDetails.bp >= 65 || exceptions_120.includes(moveName)) tempBP = 120;
 			else if (defaultDetails.bp >= 55 || exceptions_100.includes(moveName)) tempBP = 110;
 			else if (defaultDetails.bp >= 45) tempBP = 100;
-			else if (defaultDetails.bp >= 150) tempBP = 150;
 			else tempBP = 90;
 		}
 
