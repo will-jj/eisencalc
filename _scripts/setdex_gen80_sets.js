2a4b74fe89efdd309e1df4cf00be794a8864b3a9
to-metrion <20890062+to-metrion@users.noreply.github.com>
2021-11-12 17:00:22 -0500

    Update setdex_gen80_sets.js

--- _scripts/setdex_gen80_sets.js
+++ _scripts/setdex_gen80_sets.js
@@ -141,4 +141,260 @@ var SETDEX_GEN80_SETS = {
 			]
 		}
 	}
+		"Absol": {
+		"Absol-1": {
+			"level": "50",
+			"evs": {
+				"hp": 148,
+				"at": 148,
+				"df": 4,
+				"sa": 4,
+				"sd": 148,
+				"sp": 4
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 15,
+				"sa": 15,
+				"sd": 31,
+				"sp": 15
+			},
+			"nature": Careful,
+			"ability": "Pressure",
+			"item": "White Herb",
+			"moves": [
+				"Slash",
+				"Night Slash",
+				"Double Team",
+				"Taunt"
+			]
+		},
+		"Absol-2": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Adamant",
+			"ability": "Pressure",
+			"item": "Focus Sash",
+			"moves": [
+				"Swagger",
+				"Night Slash",
+				"Sucker Punch",
+				"Taunt"
+			]
+		},
+		"Absol-3": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Jolly",
+			"ability": "Super Luck",
+			"item": "Scope Lens",
+			"moves": [
+				"Night Slash",
+				"Stone Edge",
+				"Perish Song",
+				"Psycho Cut"
+			]
+		},
+		"Absol-4": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Adamant",
+			"ability": "Super Luck",
+			"item": "Scope Lens",
+			"moves": [
+				"Sucker Punch",
+				"Night Slash",
+				"Psycho Cut",
+				"Play Rough"
+			]
+		},
+	}
+	"Aerodactyl": {
+		"Aerodactyl-1": {
+			"level": "50",
+			"evs": {
+				"hp": 204,
+				"at": 60,
+				"df": 0,
+				"sa": 0,
+				"sd": 204,
+				"sp": 0
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 20,
+				"sa": 0,
+				"sd": 31,
+				"sp": 20
+			},
+			"nature": "Careful",
+			"ability": "Pressure",
+			"item": "Leftovers",
+			"moves": [
+				"Swagger",
+				"Attract",
+				"Rock Slide",
+				"Roost"
+			]
+		},
+		"Aerodactyl-2": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Jolly",
+			"ability": "Pressure",
+			"item": "Yache Berry",
+			"moves": [
+				"Aerial Ace",
+				"Crunch",
+				"Rock Slide",
+				"Earthquake"
+			]
+		},
+		"Aerodactyl-3": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Jolly",
+			"ability": "Unnerve",
+			"item": "Lum Berry",
+			"moves": [
+				"Aerial Ace",
+				"Stone Edge",
+				"Earthquake",
+				"Iron Head"
+			]
+		},
+		"Aerodactyl-4": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Jolly",
+			"ability": "Unnerve",
+			"item": "Muscle Band",
+			"moves": [
+				"Rock Slide",
+				"Iron Head",
+				"Aerial Ace",
+				"Ice Fang"
+			]
+		},
+		"Aerodactyl-5": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Jolly",
+			"ability": "Unnerve",
+			"item": "Muscle Band",
+			"moves": [
+				"Rock Slide",
+				"Earthquake",
+				"Ice Fang",
+				"Iron Head"
+			]
+		}
+	}
 };
