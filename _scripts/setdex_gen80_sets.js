35e570273807069f8a3ad665d5837a46d4d6bc40
to-metrion <20890062+to-metrion@users.noreply.github.com>
2021-11-12 16:35:50 -0500

    BDSP sets

---
+++ _scripts/setdex_gen80_sets.js
@@ -0,0 +1,144 @@
+var SETDEX_GEN80_SETS = {
+	"Abomasnow": {
+		"Abomasnow-1": {
+			"level": "50",
+			"evs": {
+				"hp": 204,
+				"at": 60,
+				"df": 0,
+				"sa": 204,
+				"sd": 0,
+				"sp": 0
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 20,
+				"sa": 31,
+				"sd": 20,
+				"sp": 20
+			},
+			"nature": "Mild",
+			"ability": "Snow Warning",
+			"item": "Occa Berry",
+			"moves": [
+				"Blizzard",
+				"Earthquake",
+				"Brick Break",
+				"Giga Drain"
+			]
+		},
+		"Abomasnow-2": {
+			"level": "50",
+			"evs": {
+				"hp": 252,
+				"at": 252,
+				"df": 4,
+				"sa": 0,
+				"sd": 0,
+				"sp": 0
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 0,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Adamant",
+			"ability": "Snow Warning",
+			"item": "Leftovers",
+			"moves": [
+				"Wood Hammer",
+				"Ingrain",
+				"Substitute",
+				"Avalanche"
+			]
+		},
+		"Abomasnow-3": {
+			"level": "50",
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 4
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 31,
+				"df": 31,
+				"sa": 31,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Modest",
+			"ability": "Snow Warning",
+			"item": "Zoom Lens",
+			"moves": [
+				"Blizzard",
+				"Sheer Cold",
+				"Focus Blast",
+				"Water Pulse"
+			]
+		},
+		"Abomasnow-4": {
+			"level": "50",
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 0,
+				"df": 31,
+				"sa": 31,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Timid",
+			"ability": "Snow Warning",
+			"item": "Choice Scarf",
+			"moves": [
+				"Blizzard",
+				"Energy Ball",
+				"Earth Power",
+				"Focus Blast"
+			]
+		},
+		"Abomasnow-5": {
+			"level": "50",
+			"evs": {
+				"hp": 252,
+				"at": 4,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 0
+			},
+			"ivs": {
+				"hp": 31,
+				"at": 0,
+				"df": 31,
+				"sa": 31,
+				"sd": 31,
+				"sp": 31
+			},
+			"nature": "Modest",
+			"ability": "Snow Warning",
+			"item": "Icy Rock",
+			"moves": [
+				"Blizzard",
+				"Giga Drain",
+				"Earth Power",
+				"Focus Blast"
+			]
+		}
+	}
+};
