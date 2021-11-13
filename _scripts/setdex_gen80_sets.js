f99b77e5782b01c5f83764f4f5289f7ed5f0cd3e
to-metrion <20890062+to-metrion@users.noreply.github.com>
2021-11-12 22:38:33 -0500

    A

--- _scripts/setdex_gen80_sets.js
+++ _scripts/setdex_gen80_sets.js
@@ -645,5 +645,836 @@ var SETDEX_GEN80_SETS = {
 "Protect"
 ]
 }
-	}
+	},
+	"Altaria": {
+		"Altaria-1": {
+			"level": "50",
+"evs": {
+"hp": 204,
+"at": 0,
+"df": 0,
+"sa": 60,
+"sd": 204,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 20,
+"sa": 31,
+"sp": 31
+},
+"nature": "Calm",
+"ability": "Cloud Nine",
+"item": "Petaya Berry",
+"moves": [
+"Dragon Pulse",
+"Roost",
+"Perish Song",
+"Protect"
+]
+},
+		"Altaria-2": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Modest",
+"ability": "Cloud Nine",
+"item": "White Herb",
+"moves": [
+"Draco Meteor",
+"Hyper Beam",
+"Ice Beam",
+"Flamethrower"
+]
+},
+		"Altaria-3": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Modest",
+"ability": "Cloud Nine",
+"item": "Dragon Fang",
+"moves": [
+"Roost",
+"Dragon Pulse",
+"Hyper Voice",
+"Dragon Claw"
+]
+}
+	},
+	"Ambipom": {
+		"Ambipom-1": {
+			"level": "50",
+"evs": {
+"hp": 148,
+"at": 148,
+"df": 148,
+"sa": 4,
+"sd": 4,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 15,
+"sp": 15
+},
+"nature": "Impish",
+"ability": "Technician",
+"item": "Bright Powder",
+"moves": [
+"Double Team",
+"Baton Pass",
+"Agility",
+"Double Hit"
+]
+},
+		"Ambipom-2": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Modest",
+"ability": "Technician",
+"item": "Wise Glasses",
+"moves": [
+"Shadow Ball",
+"Thunderbolt",
+"Swift",
+"Nasty Plot"
+]
+},
+		"Ambipom-3": {
+			"level": "50",
+"evs": {
+"hp": 20,
+"at": 244,
+"df": 84,
+"sa": 0,
+"sd": 4,
+"sp": 156
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Jolly",
+"ability": "Technician",
+"item": "Muscle Band",
+"moves": [
+"Double Hit",
+"Thief",
+"Dual Chop",
+"Fake Out"
+]
+}
+	},
+"Ampharos": {
+		"Ampharos-1": {
+			"level": "50",
+"evs": {
+"hp": 204,
+"at": 0,
+"df": 60,
+"sa": 0,
+"sd": 204,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 20,
+"df": 31,
+"sa": 20,
+"sp": 31
+},
+"nature": "Mild",
+"ability": "Static",
+"item": "Toxic Orb",
+"moves": [
+"Thunder Wave",
+"Facade",
+"Fling",
+"Rest"
+]
+},
+		"Ampharos-2": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 4,
+"df": 0,
+"sa": 0,
+"sd": 252,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 0,
+"sp": 31
+},
+"nature": "Careful",
+"ability": "Static",
+"item": "Light Clay",
+"moves": [
+"Reflect",
+"Double Team",
+"Focus Punch",
+"Thunder Punch"
+]
+},
+		"Ampharos-3": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 252,
+"sa": 0,
+"sd": 4,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Bold",
+"ability": "Static",
+"item": "Chesto Berry",
+"moves": [
+"Thunder Wave",
+"Charge Beam",
+"Dragon Pulse",
+"Rest"
+]
+}
+	},
+"Anorith": {
+		"Anorith-1": {
+			"level": "50",
+"evs": {
+"hp": 0,
+"at": 108,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 108
+},
+"ivs": {
+"hp": 0,
+"at": 31,
+"df": 0,
+"sa": 0,
+"sp": 0
+},
+"nature": "Adamant",
+"ability": "Battle Armor",
+"item": "Scope Lens",
+"moves": [
+"Metal Claw",
+"Fury Cutter",
+"Ancient Power",
+"Water Gun"
+]
+}
+	},
+"Arbok": {
+		"Arbok-1": {
+			"level": "50",
+"evs": {
+"hp": 108,
+"at": 108,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 108
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 0,
+"sa": 0,
+"sp": 0
+},
+"nature": "Adamant",
+"ability": "Intimidate",
+"item": "Persim Berry",
+"moves": [
+"Poison Fang",
+"Dig",
+"Bite",
+"Glare"
+]
+}
+	},
+"Arcanine": {
+		"Arcanine-1": {
+			"level": "50",
+"evs": {
+"hp": 68,
+"at": 0,
+"df": 0,
+"sa": 220,
+"sd": 0,
+"sp": 220
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 25,
+"sa": 31,
+"sp": 25
+},
+"nature": "Modest",
+"ability": "Intimidate",
+"item": "Lum Berry",
+"moves": [
+"Flamethrower",
+"Dragon Pulse",
+"Will-O-Wisp",
+"Extreme Speed"
+]
+},
+		"Arcanine-2": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 252,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 0,
+"sp": 31
+},
+"nature": "Jolly",
+"ability": "Intimidate",
+"item": "Salac Berry",
+"moves": [
+"Endure",
+"Reversal",
+"Flare Blitz",
+"Play Rough"
+]
+},
+		"Arcanine-3": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 116,
+"sa": 0,
+"sd": 0,
+"sp": 140
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Bold",
+"ability": "Intimidate",
+"item": "Leftovers",
+"moves": [
+"Flamethrower",
+"Dragon Pulse",
+"Will-O-Wisp",
+"Morning Sun"
+]
+},
+		"Arcanine-4": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 252,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 0,
+"sp": 31
+},
+"nature": "Adamant",
+"ability": "Intimidate",
+"item": "Sitrus Berry",
+"moves": [
+"Flare Blitz",
+"Wild Charge",
+"Play Rough",
+"Will-O-Wisp"
+]
+},
+		"Arcanine-5": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 0,
+"sd": 36,
+"sp": 220
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Timid",
+"ability": "Intimidate",
+"item": "Wiki Berry",
+"moves": [
+"Heat Wave",
+"Snarl",
+"Will-O-Wisp",
+"Flamethrower"
+]
+},
+		"Arcanine-6": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Timid",
+"ability": "Intimidate",
+"item": "Wiki Berry",
+"moves": [
+"Heat Wave",
+"Snarl",
+"Will-O-Wisp",
+"Flamethrower"
+]
+}
+	},
+"Ariados": {
+		"Ariados-1": {
+			"level": "50",
+"evs": {
+"hp": 108,
+"at": 0,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 108
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 0,
+"sa": 0,
+"sp": 0
+},
+"nature": "Timid",
+"ability": "Insomnia",
+"item": "Focus Sash",
+"moves": [
+"Sticky Web",
+"Toxic Spikes",
+"Night Shade",
+"Toxic Thread"
+]
+}
+},
+"Armaldo": {
+		"Armaldo-1": {
+			"level": "50",
+"evs": {
+"hp": 204,
+"at": 60,
+"df": 0,
+"sa": 0,
+"sd": 204,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 20,
+"sa": 0,
+"sp": 31
+},
+"nature": "Careful",
+"ability": "Battle Armor",
+"item": "Wide Lens",
+"moves": [
+"Stone Edge",
+"Slash",
+"X-Scissor",
+"Iron Tail"
+]
+},
+		"Armaldo-2": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 252,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 0,
+"sp": 31
+},
+"nature": "Jolly",
+"ability": "Battle Armor",
+"item": "Focus Sash",
+"moves": [
+"Rock Polish",
+"Rock Blast",
+"Cross Poison",
+"X-Scissor"
+]
+},
+		"Armaldo-3": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 252,
+"df": 4,
+"sa": 0,
+"sd": 0,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Brave",
+"ability": "Battle Armor",
+"item": "Focus Band",
+"moves": [
+"Sandstorm",
+"Brick Break",
+"Aerial Ace",
+"Giga Impact"
+]
+}
+	},	
+"Aron": {
+		"Aron-1": {
+			"level": "50",
+"evs": {
+"hp": 108,
+"at": 108,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 0,
+"sa": 0,
+"sp": 0
+},
+"nature": "Adamant",
+"ability": "Sturdy",
+"item": "Metal Coat",
+"moves": [
+"Metal Claw",
+"Aerial Ace",
+"Metal Sound",
+"Roar"
+]
+}
+"Articuno": {
+		"Articuno-1": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 0,
+"sd": 252,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Calm",
+"ability": "Snow Cloak",
+"item": "Icy Rock",
+"moves": [
+"Swagger",
+"Hail",
+"Roost",
+"Blizzard"
+]
+},
+		"Articuno-2": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Modest",
+"ability": "Pressure",
+"item": "Chesto Berry",
+"moves": [
+"Agility",
+"Ice Beam",
+"Water Pulse",
+"Rest"
+]
+},
+		"Articuno-3": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 4,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Timid",
+"ability": "Pressure",
+"item": "Bright Powder",
+"moves": [
+"Roost",
+"Ice Beam",
+"Sheer Cold",
+"Double Team"
+]
+},
+		"Articuno-4": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 4,
+"sd": 252,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Calm",
+"ability": "Pressure",
+"item": "Wacan Berry",
+"moves": [
+"Sheer Cold",
+"Ice Beam",
+"Aerial Ace",
+"Roar"
+]
+},
+		"Articuno-5": {
+			"level": "50",
+"evs": {
+"hp": 4,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 252
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Timid",
+"ability": "Snow Cloak",
+"item": "Choice Specs",
+"moves": [
+"Blizzard",
+"Hurricane",
+"Freeze-Dry",
+"Ancient Power"
+]
+},
+		"Articuno-6": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 0,
+"df": 0,
+"sa": 252,
+"sd": 0,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 0,
+"df": 31,
+"sa": 31,
+"sp": 31
+},
+"nature": "Modest",
+"ability": "Pressure",
+"item": "Leftovers",
+"moves": [
+"Blizzard",
+"Hurricane",
+"Roost",
+"Sheer Cold"
+]
+}	
+},
+"Azumarill": {
+		"Azumarill-1": {
+			"level": "50",
+"evs": {
+"hp": 108,
+"at": 108,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 0
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 0,
+"sa": 0,
+"sp": 0
+},
+"nature": "Adamant",
+"ability": "Huge Power",
+"item": "Sitrus Berry",
+"moves": [
+"Waterfall",
+"Facade",
+"Play Rough",
+"Aqua Jet"
+]
+},
+		"Azumarill-2": {
+			"level": "50",
+"evs": {
+"hp": 252,
+"at": 252,
+"df": 0,
+"sa": 0,
+"sd": 0,
+"sp": 4
+},
+"ivs": {
+"hp": 31,
+"at": 31,
+"df": 31,
+"sa": 0,
+"sp": 31
+},
+"nature": "Adamant",
+"ability": "Huge Power",
+"item": "Sitrus Berry",
+"moves": [
+"Aqua Jet",
+"Play Rough",
+"Superpower",
+"Belly Drum"
+]
+}
+	}	
 };
