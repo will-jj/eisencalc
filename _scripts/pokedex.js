7d36ff23cf47f4698925850521d226b18a4dfa1e
to-metrion <20890062+to-metrion@users.noreply.github.com>
2022-11-20 16:43:35 -0500

    Add PLA Pokémon

--- _scripts/pokedex.js
+++ _scripts/pokedex.js
@@ -16424,7 +16424,332 @@ var POKEDEX_SV = $.extend(true, {}, POKEDEX_SS, {
 	"Zacian": {"bs": {"at": 120}},
 	"Zacian-Crowned": {"bs": {"at": 150}},
 	"Zamazenta": {"bs": {"at": 120}},
-	"Zamazenta-Crowned": {"bs": {"at": 120, "df": 140, "sd": 140}}
+	"Zamazenta-Crowned": {"bs": {"at": 120, "df": 140, "sd": 140}},
+	"Wyrdeer": {
+		"t1": "Normal",
+		"t2": "Psychic",
+		"bs": {
+			"hp": 103,
+			"at": 105,
+			"df": 72,
+			"sa": 105,
+			"sd": 75,
+			"sp": 65
+		},
+		"w": 95.1
+	},
+	"Kleavor": {
+		"t1": "Bug",
+		"t2": "Rock",
+		"bs": {
+			"hp": 70,
+			"at": 135,
+			"df": 95,
+			"sa": 45,
+			"sd": 70,
+			"sp": 85
+		},
+		"w": 89
+	},
+	"Ursaluna": {
+		"t1": "Ground",
+		"t2": "Normal",
+		"bs": {
+			"hp": 130,
+			"at": 140,
+			"df": 105,
+			"sa": 45,
+			"sd": 80,
+			"sp": 50
+		},
+		"w": 290
+	},
+	"Basculegion-M": {
+		"t1": "Water",
+		"t2": "Ghost",
+		"bs": {
+			"hp": 120,
+			"at": 112,
+			"df": 65,
+			"sa": 80,
+			"sd": 75,
+			"sp": 78
+		},
+		"w": 110
+	},
+	"Basculegion-F": {
+		"t1": "Water",
+		"t2": "Ghost",
+		"bs": {
+			"hp": 120,
+			"at": 92,
+			"df": 65,
+			"sa": 100,
+			"sd": 75,
+			"sp": 78
+		},
+		"w": 110
+	},
+	"Sneasler": {
+		"t1": "Poison",
+		"t2": "Fighting",
+		"bs": {
+			"hp": 80,
+			"at": 130,
+			"df": 60,
+			"sa": 40,
+			"sd": 80,
+			"sp": 120
+		},
+		"w": 43
+	},
+	"Overqwil": {
+		"t1": "Dark",
+		"t2": "Poison",
+		"bs": {
+			"hp": 85,
+			"at": 115,
+			"df": 95,
+			"sa": 65,
+			"sd": 65,
+			"sp": 85
+		},
+		"w": 60.5
+	},
+	"Enamorus": {
+		"t1": "Fairy",
+		"t2": "Flying",
+		"bs": {
+			"hp": 74,
+			"at": 115,
+			"df": 70,
+			"sa": 135,
+			"sd": 80,
+			"sp": 106
+		},
+		"w": 48
+	},
+	"Enamorus-T": {
+		"t1": "Fairy",
+		"t2": "Flying",
+		"bs": {
+			"hp": 74,
+			"at": 115,
+			"df": 110,
+			"sa": 135,
+			"sd": 100,
+			"sp": 46
+		},
+		"w": 110
+	},
+	"Growlithe-Hisui": {
+		"t1": "Fire",
+		"t2": "Rock",
+		"bs": {
+			"hp": 60,
+			"at": 75,
+			"df": 45,
+			"sa": 65,
+			"sd": 50,
+			"sp": 55
+		},
+		"w": 22.7
+	},
+	"Arcanine-Hisui": {
+		"t1": "Fire",
+		"t2": "Rock",
+		"bs": {
+			"hp": 95,
+			"at": 115,
+			"df": 80,
+			"sa": 95,
+			"sd": 80,
+			"sp": 90
+		},
+		"w": 168
+	},
+	"Voltorb-Hisui": {
+		"t1": "Electric",
+		"t2": "Grass",
+		"bs": {
+			"hp": 40,
+			"at": 30,
+			"df": 50,
+			"sa": 55,
+			"sd": 55,
+			"sp": 100
+		},
+		"w": 13
+	},
+	"Electrode-Hisui": {
+		"t1": "Electric",
+		"t2": "Grass",
+		"bs": {
+			"hp": 60,
+			"at": 50,
+			"df": 70,
+			"sa": 80,
+			"sd": 80,
+			"sp": 150
+		},
+		"w": 71
+	},
+	"Typhlosion-Hisui": {
+		"t1": "Fire",
+		"t2": "Ghost",
+		"bs": {
+			"hp": 73,
+			"at": 84,
+			"df": 78,
+			"sa": 119,
+			"sd": 85,
+			"sp": 95
+		},
+		"w": 69.8
+	},
+	"Qwilfish-Hisui": {
+		"t1": "Dark",
+		"t2": "Poison",
+		"bs": {
+			"hp": 65,
+			"at": 95,
+			"df": 85,
+			"sa": 55,
+			"sd": 55,
+			"sp": 85
+		},
+		"w": 3.9
+	},
+	"Sneasel-Hisui": {
+		"t1": "Poison",
+		"t2": "Fighting",
+		"bs": {
+			"hp": 55,
+			"at": 95,
+			"df": 55,
+			"sa": 35,
+			"sd": 75,
+			"sp": 115
+		},
+		"w": 27
+	},
+	"Samurott-Hisui": {
+		"t1": "Water",
+		"t2": "Dark",
+		"bs": {
+			"hp": 90,
+			"at": 108,
+			"df": 80,
+			"sa": 100,
+			"sd": 65,
+			"sp": 85
+		},
+		"w": 58.2
+	},
+	"Lilligant-Hisui": {
+		"t1": "Grass",
+		"t2": "Fighting",
+		"bs": {
+			"hp": 70,
+			"at": 105,
+			"df": 75,
+			"sa": 50,
+			"sd": 75,
+			"sp": 105
+		},
+		"w": 19.2
+	},
+	"Zorua-Hisui": {
+		"t1": "Normal",
+		"t2": "Ghost",
+		"bs": {
+			"hp": 35,
+			"at": 60,
+			"df": 40,
+			"sa": 85,
+			"sd": 40,
+			"sp": 70
+		},
+		"w": 12.5
+	},
+	"Zoroark-Hisui": {
+		"t1": "Normal",
+		"t2": "Ghost",
+		"bs": {
+			"hp": 55,
+			"at": 100,
+			"df": 60,
+			"sa": 125,
+			"sd": 60,
+			"sp": 110
+		},
+		"w": 73
+	},
+	"Braviary-Hisui": {
+		"t1": "Psychic",
+		"t2": "Flying",
+		"bs": {
+			"hp": 110,
+			"at": 83,
+			"df": 70,
+			"sa": 112,
+			"sd": 70,
+			"sp": 65
+		},
+		"w": 43.4
+	},
+	"Sliggoo-Hisui": {
+		"t1": "Steel",
+		"t2": "Dragon",
+		"bs": {
+			"hp": 58,
+			"at": 75,
+			"df": 83,
+			"sa": 83,
+			"sd": 113,
+			"sp": 40
+		},
+		"w": 68.5
+	},
+	"Goodra-Hisui": {
+		"t1": "Steel",
+		"t2": "Dragon",
+		"bs": {
+			"hp": 80,
+			"at": 100,
+			"df": 100,
+			"sa": 110,
+			"sd": 150,
+			"sp": 60
+		},
+		"w": 334.1
+	},
+	"Avalugg-Hisui": {
+		"t1": "Ice",
+		"t2": "Rock",
+		"bs": {
+			"hp": 95,
+			"at": 127,
+			"df": 184,
+			"sa": 34,
+			"sd": 36,
+			"sp": 38
+		},
+		"w": 262.4
+	},
+	"Decidueye-Hisui": {
+		"t1": "Grass",
+		"t2": "Fighting",
+		"bs": {
+			"hp": 88,
+			"at": 112,
+			"df": 80,
+			"sa": 95,
+			"sd": 95,
+			"sp": 60
+		},
+		"w": 37
+	}
 });
 
 // Remove Megas for SwSh
